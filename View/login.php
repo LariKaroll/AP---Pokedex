6dbe20f100a95b35050ec71c9885e34d1d9aaf0d
LariKaroll <larikarol108@gmail.com>
2024-11-18 20:06:06 -0300

    Php - pokedex

---
+++ View/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/pokeball.png" type="image/x-icon">
+    <title>Login</title>
+</head>
+<body>
+    <H1>Login</H1>
+    <p><input type="text" name="username" id="username" placeholder="Username"></p>
+    <p><input type="password" name="password" id="password" placeholder="Password"></p>
+    <p><input type="submit" value="Login"></p>
+</body>
+</html>
